8ab129a720b0a8fb7fd59d45ce976c836006b6d5
llopez9047 <llopez9047@utm.edu.ec>
2026-08-02 18:07:03 -0500

    Rename '/register' route to '/registrar'
    Control de rutas

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.registrar);
+router.post('/registrar', authController.registrar);
 router.post('/login', authController.login);
 
 module.exports = router;
